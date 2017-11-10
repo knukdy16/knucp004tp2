2b04558b71354498aa85d2b9551b8b3e3bfbb9c9
knukdy16 <join262@naver.com>
2017-11-10 15:38:47 +0900

    Create file.c

---
+++ file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void readfile(int a[], int* size)
+{
+        FILE *fp;
+        char fname[20] = "input.txt";
+        int i = 1;
+
+        if((fp = fopen(fname, "r")) == NULL)
+        {
+                fprintf(stderr, "File is not open\n");
+                exit(1);
+        }
+
+        while(!feof(fp))
+        {
+                fscanf(fp, "%d", &a[i++]);
+                (*size)++;
+        }
+        (*size)--;
+
+        fclose(fp);
+}
