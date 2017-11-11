5598383f25f444101e615894fc1df48c378d5c1d
knukdy16 <join262@naver.com>
2017-11-11 17:38:15 +0900

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sorting.h"
 
-void insert(int e, int a[], int i )
-{
+void insert(int e, int a[], int i ) {
         a[0] = e;
-        while(e < a[i])
-        {
+        while(e < a[i]) {
                 a[i+1] = a[i];
                 i--;
         }
         a[i+1] = e;
 }
 
-void insertionSort(int a[], int n)
-{
+void insertionSort(int a[], int n) {
         int j, temp;
-        for(j=2;j<=n;j++)
-        {
+        for(j=2;j<=n-1;j++) {
                 temp = a[j];
                 insert(temp, a, j-1);
         }
 }
+/*
+int main() {
+        int i;
+        int a[100 + 1];
+        for(i = 1; i < 101; i++) {
+                a[i] = 100 + 1 - i;
+                printf("%d ", a[i]);
+        }
+
+        insertionSort(a, 100 + 1);
+        printf("\n Sorted Array:\n ");
+        for(i = 1; i < 100 + 1; i++) {
+                printf("%d ", a[i]);
+        }
+        puts("");
+}*/
