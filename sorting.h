1f1579e6333e3aa59b54750c248dc60b45ed3ea0
knukdy16 <join262@naver.com>
2017-11-11 17:35:31 +0900

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,6 +1,7 @@
-#define MAX_SIZE 1000001
-
 void insert(int, int[], int);
 void insertionSort(int[], int);
 void quickSort(int[], int, int);
-void readfile(int[], int*);
+int *load_ary(char *[], int *);
+int *set_arr2(int *, int);
+int *set_arr3(int *, int);
+void oops(char *, char *);
