939ed537125d09491f62f7314b8b38c54c0a12e8
knukdy16 <join262@naver.com>
2017-11-11 17:34:21 +0900

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -3,31 +3,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/time.h>
-#include <limits.h>
 #include "sorting.h"
 
-void main()
-{
-        int sizeA = 0, sizeB = 0, i = 1;
-        int a[MAX_SIZE] = {0}, b[MAX_SIZE] = {0};
+void main(int argc, char* argv[]) {
+        int size = 0; // store array size
+        int *ary_size = &size; // pointer to save array size
+                               // from load_ary
+        int *arr1, *arr2, *arr3; // arr1 saves original.
+                                 // arr2 saves insertion.
+                                 // arr3 saves quick.
+        int i;
         struct timeval t1, t2;
 
-        readfile(a, &sizeA);
-        readfile(b, &sizeB);
-        b[sizeB+1] = INT_MAX;
+        arr1 = load_ary(argv, ary_size);
+/*
+        puts("loaded <arr1>");
+        for(i = 0; i < size; i++) {
+                printf("%d %d\n", i, arr1[i]);
+        }
+*/
+        arr2 = set_arr2(arr1, size);
+        arr3 = set_arr3(arr1, size);
 
         gettimeofday(&t1, NULL);
-        insertionSort(a, sizeA);
+        insertionSort(arr2, size + 1);
         gettimeofday(&t2, NULL);
-
-        printf("\n");
-        printf("%.0lf milliseconds\n", (t2.tv_sec*1000 + t2.tv_usec*0.001) - (t1.tv_sec*1000 + t1.tv_usec*0.001));
+        printf("%0.lf miliseconds\n", (t2.tv_sec*1000 + t2.tv_usec*0.001) - (t1.tv_sec*1000 + t1.tv_usec*0.001));
 
         gettimeofday(&t1, NULL);
-        printf("before quicksort\n");
-        quickSort(a, 1, sizeB);
-        printf("after quicksort\n");
+        quickSort(arr3, 0, size - 1);
         gettimeofday(&t2, NULL);
-        printf("%.01f milliseconds\n", (t2.tv_sec*100 + t2.tv_usec*0.001) - (t1.tv_sec*1000 + t1.tv_usec*0.001));
+        printf("%0.lf miliseconds\n", (t2.tv_sec*1000 + t2.tv_usec*0.001) - (t1.tv_sec*1000 + t1.tv_usec*0.001));
 
 }
